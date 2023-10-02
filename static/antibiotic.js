275c94a2977ee4ce6408fe35c8ad7c5dd6aa6d19
Nick Ng <nick.nghn@gmail.com>
2023-10-02 12:02:46 +1100

    Add files via upload

---
+++ static/antibiotic.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let button1 = document.querySelector('.home');
+    button1.addEventListener('click',function(){
+        location.href="https://fictional-happiness-55vppgvr4gw2rwp-5000.app.github.dev/";
+    });
+
+    let button2 = document.querySelector('.submit');
+    button2.addEventListener('click',function(){
+        alert("Disclaimer: This child oral antibiotic calculator is provided in good faith. While its contents and calculations have been verified, the calculator is provided 'as-is' and without any support or warranty of any kind.");
+    });
+});
